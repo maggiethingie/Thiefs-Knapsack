2be78f9cc17879f79af1e62c36e41d7b50775d39
Maggie Aytac <maggie.aytac@gmail.com>
2021-04-14 12:34:01 -0700

    second solution

--- solution.js
+++ solution.js
@@ -8,29 +8,18 @@ The Thief can't pick the same item twice.
 What is the maximum worth of goods that the thief can steal?
 */
 
-/* each item can be represented as an object:
-item = {
-  val:
-  weight:
-}
+/* input: 1) num lbs knapsack can carry and 2) set of possible goods to be stolen
+   output: maximum total value of goods thief can take
 
-knapsack can be represented as a Set of items (because in Set each is unique)
-
-totalWeight must be <= X
-totalVal must be optimized
+x = Number of lbs thief's knapsack can hold
+items = array of possible stolen goods thief might choose to steal (where each item
+is an object with val property and weight property
 */
 
-// input: 1) num lbs knapsack can carry and 2) set of possible goods to be stolen
-// output: maximum total value of goods thief can take
-
-// x = Number of lbs thief's knapsack can hold
-// items = Set of possible stolen goods thief might choose to steal, where each item
-// is an object with val property and weight property
-
-function thiefsKnapsack (x, items) {
-  // sort items by value
-  const sorted = items.sort((a, b) => a.val < b.val ? -1 : 1);
-
+// solution 1 (passes 2/3 of my tests):
+function thiefsKnapsack1 (x, items) {
+  const sorted = items.sort((a, b) => a.val > b.val ? -1 : 1);
+  //console.log(sorted);
   let groupVals = [];
 
   let n = items.length;
@@ -50,10 +39,45 @@ function thiefsKnapsack (x, items) {
   }
 
   groupVals = groupVals.sort((a, b) => a > b ? -1 : 1);
-  console.log(groupVals);
+  //console.log(groupVals);
   return groupVals[0];
 }
 
+//solution 2 (passes 3/3 of my tests but perhaps my tests aren't thorough enough):
+function thiefsKnapsack2 (x, items) {
+  items = items.sort((a, b) => a.val < b.val ? -1 : 1);
+
+  let totalWeight = getWeight(items);
+  let totalVal = getVal(items);
+  let groupVals = [];
+
+  while (totalWeight > x) {
+    //console.log(items);
+    for (let i = 0; i < items.length; i ++) {
+      let tempItems = items.slice(0, i).concat(items.slice(i + 1));
+      if (getWeight(tempItems) <= x) {
+        groupVals.push(getVal(tempItems));
+      }
+    }
+    totalWeight -= items[0].weight;
+    totalVal -= items[0].val;
+    items = items.slice(1);
+  }
+
+  groupVals = groupVals.sort((a, b) => a > b ? -1 : 1);
+  //console.log(groupVals);
+  return groupVals[0];
+}
+
+function getWeight (items) {
+  return items.reduce((acc, cur) => acc + cur.weight, 0);
+}
+
+function getVal (items) {
+  return items.reduce((acc, cur) => acc + cur.val, 0);
+}
+
+console.log('* TESTS *');
 let items = [
   { val: 5, weight: 5 },
   { val: 2, weight: 1 },
@@ -61,4 +85,22 @@ let items = [
   { val: 6, weight: 9 }
 ];
 
-console.log(thiefsKnapsack(10, items));
+console.log(thiefsKnapsack2(10, items), 'should be 9');
+
+items = [
+  { val: 5, weight: 5 },
+  { val: 20, weight: 1 },
+  { val: 2, weight: 1 },
+  { val: 5, weight: 8 }
+];
+
+console.log(thiefsKnapsack2(10, items), 'should be 27');
+
+items = [
+  { val: 14, weight: 2 },
+  { val: 7, weight: 4 },
+  { val: 9, weight: 5 },
+  { val: 23, weight: 6 }
+];
+
+console.log(thiefsKnapsack2(10, items), 'should be 37');
